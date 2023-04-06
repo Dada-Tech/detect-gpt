a13cbcbcd5a2cf904b3074868122a2fc243bfc24
dada-tech <david.olu.dada@gmail.com>
2023-04-05 21:04:47 -0600

    main to run all. added output_name to each run

--- paper_scripts/main.sh
+++ paper_scripts/main.sh
@@ -1,59 +1,18 @@
-!python run.py --batch_size 5 --n_samples 100 --n_perturbation_list 1,3 --base_model_name gpt2 --mask_filling_model_name t5-small --dataset reviews --cache_dir models
-
-#python run.py --output_name main --base_model_name gpt2-xl --mask_filling_model_name t5-3b --n_perturbation_list 1,10,100 --n_samples 500 --pct_words_masked 0.3 --span_length 2
-#python run.py --output_name main --base_model_name EleutherAI/gpt-neo-2.7B --mask_filling_model_name t5-3b --n_perturbation_list 1,10,100 --n_samples 500 --pct_words_masked 0.3 --span_length 2
-#python run.py --output_name main --base_model_name EleutherAI/gpt-j-6B --mask_filling_model_name t5-3b --n_perturbation_list 1,10,100 --n_samples 500 --pct_words_masked 0.3 --span_length 2
-#python run.py --output_name main --base_model_name facebook/opt-2.7b --mask_filling_model_name t5-3b --n_perturbation_list 1,10,100 --n_samples 500 --pct_words_masked 0.3 --span_length 2
-#python run.py --output_name main --batch_size 20 --base_model_name EleutherAI/gpt-neox-20b --mask_filling_model_name t5-11b --n_perturbation_list 1,10,100 --n_samples 500 --pct_words_masked 0.3 --span_length 2
-#
-#python run.py --output_name main_top_p --base_model_name gpt2-xl --mask_filling_model_name t5-3b --n_perturbation_list 1,10,100 --n_samples 500 --pct_words_masked 0.3 --span_length 2 --do_top_p
-#python run.py --output_name main_top_p --base_model_name EleutherAI/gpt-neo-2.7B --mask_filling_model_name t5-3b --n_perturbation_list 1,10,100 --n_samples 500 --pct_words_masked 0.3 --span_length 2 --do_top_p
-#python run.py --output_name main_top_p --base_model_name EleutherAI/gpt-j-6B --mask_filling_model_name t5-3b --n_perturbation_list 1,10,100 --n_samples 500 --pct_words_masked 0.3 --span_length 2 --do_top_p
-#python run.py --output_name main_top_p --base_model_name facebook/opt-2.7b --mask_filling_model_name t5-3b --n_perturbation_list 1,10,100 --n_samples 500 --pct_words_masked 0.3 --span_length 2 --do_top_p
-#python run.py --output_name main_top_p --batch_size 20 --base_model_name EleutherAI/gpt-neox-20b --mask_filling_model_name t5-11b --n_perturbation_list 1,10,100 --n_samples 500 --pct_words_masked 0.3 --span_length 2 --do_top_p
-#
-#python run.py --output_name main_top_k --base_model_name gpt2-xl --mask_filling_model_name t5-3b --n_perturbation_list 1,10,100 --n_samples 500 --pct_words_masked 0.3 --span_length 2 --do_top_k
-#python run.py --output_name main_top_k --base_model_name EleutherAI/gpt-neo-2.7B --mask_filling_model_name t5-3b --n_perturbation_list 1,10,100 --n_samples 500 --pct_words_masked 0.3 --span_length 2 --do_top_k
-#python run.py --output_name main_top_k --base_model_name EleutherAI/gpt-j-6B --mask_filling_model_name t5-3b --n_perturbation_list 1,10,100 --n_samples 500 --pct_words_masked 0.3 --span_length 2 --do_top_k
-#python run.py --output_name main_top_k --base_model_name facebook/opt-2.7b --mask_filling_model_name t5-3b --n_perturbation_list 1,10,100 --n_samples 500 --pct_words_masked 0.3 --span_length 2 --do_top_k
-#python run.py --output_name main_top_k --batch_size 20 --base_model_name EleutherAI/gpt-neox-20b --mask_filling_model_name t5-11b --n_perturbation_list 1,10,100 --n_samples 500 --pct_words_masked 0.3 --span_length 2 --do_top_k
-#
-#
-#
-#python run.py --output_name main --base_model_name gpt2-xl --mask_filling_model_name t5-3b --n_perturbation_list 1,10,100 --n_samples 312 --pct_words_masked 0.3 --span_length 2 --dataset squad --dataset_key context
-#python run.py --output_name main --base_model_name EleutherAI/gpt-neo-2.7B --mask_filling_model_name t5-3b --n_perturbation_list 1,10,100 --n_samples 312 --pct_words_masked 0.3 --span_length 2 --dataset squad --dataset_key context
-#python run.py --output_name main --base_model_name EleutherAI/gpt-j-6B --mask_filling_model_name t5-3b --n_perturbation_list 1,10,100 --n_samples 312 --pct_words_masked 0.3 --span_length 2 --dataset squad --dataset_key context
-#python run.py --output_name main --base_model_name facebook/opt-2.7b --mask_filling_model_name t5-3b --n_perturbation_list 1,10,100 --n_samples 312 --pct_words_masked 0.3 --span_length 2 --dataset squad --dataset_key context
-#python run.py --output_name main --batch_size 20 --base_model_name EleutherAI/gpt-neox-20b --mask_filling_model_name t5-11b --n_perturbation_list 1,10,100 --n_samples 312 --pct_words_masked 0.3 --span_length 2 --dataset squad --dataset_key context
-#
-#python run.py --output_name main_top_p --base_model_name gpt2-xl --mask_filling_model_name t5-3b --n_perturbation_list 1,10,100 --n_samples 312 --pct_words_masked 0.3 --span_length 2 --do_top_p --dataset squad --dataset_key context
-#python run.py --output_name main_top_p --base_model_name EleutherAI/gpt-neo-2.7B --mask_filling_model_name t5-3b --n_perturbation_list 1,10,100 --n_samples 312 --pct_words_masked 0.3 --span_length 2 --do_top_p --dataset squad --dataset_key context
-#python run.py --output_name main_top_p --base_model_name EleutherAI/gpt-j-6B --mask_filling_model_name t5-3b --n_perturbation_list 1,10,100 --n_samples 312 --pct_words_masked 0.3 --span_length 2 --do_top_p --dataset squad --dataset_key context
-#python run.py --output_name main_top_p --base_model_name facebook/opt-2.7b --mask_filling_model_name t5-3b --n_perturbation_list 1,10,100 --n_samples 312 --pct_words_masked 0.3 --span_length 2 --do_top_p --dataset squad --dataset_key context
-#python run.py --output_name main_top_p --batch_size 20 --base_model_name EleutherAI/gpt-neox-20b --mask_filling_model_name t5-11b --n_perturbation_list 1,10,100 --n_samples 312 --pct_words_masked 0.3 --do_top_p --span_length 2 --dataset squad --dataset_key context
-#
-#python run.py --output_name main_top_k --base_model_name gpt2-xl --mask_filling_model_name t5-3b --n_perturbation_list 1,10,100 --n_samples 312 --pct_words_masked 0.3 --span_length 2 --do_top_k --dataset squad --dataset_key context
-#python run.py --output_name main_top_k --base_model_name EleutherAI/gpt-neo-2.7B --mask_filling_model_name t5-3b --n_perturbation_list 1,10,100 --n_samples 312 --pct_words_masked 0.3 --span_length 2 --do_top_k --dataset squad --dataset_key context
-#python run.py --output_name main_top_k --base_model_name EleutherAI/gpt-j-6B --mask_filling_model_name t5-3b --n_perturbation_list 1,10,100 --n_samples 312 --pct_words_masked 0.3 --span_length 2 --do_top_k --dataset squad --dataset_key context
-#python run.py --output_name main_top_k --base_model_name facebook/opt-2.7b --mask_filling_model_name t5-3b --n_perturbation_list 1,10,100 --n_samples 312 --pct_words_masked 0.3 --span_length 2 --do_top_k --dataset squad --dataset_key context
-#python run.py --output_name main_top_k --batch_size 20 --base_model_name EleutherAI/gpt-neox-20b --mask_filling_model_name t5-11b --n_perturbation_list 1,10,100 --n_samples 312 --pct_words_masked 0.3 --do_top_k --span_length 2 --dataset squad --dataset_key context
-#
-#
-#
-#python run.py --output_name main --base_model_name gpt2-xl --mask_filling_model_name t5-3b --n_perturbation_list 1,10,100 --n_samples 500 --pct_words_masked 0.3 --span_length 2 --dataset writing
-#python run.py --output_name main --base_model_name EleutherAI/gpt-neo-2.7B --mask_filling_model_name t5-3b --n_perturbation_list 1,10,100 --n_samples 500 --pct_words_masked 0.3 --span_length 2 --dataset writing
-#python run.py --output_name main --base_model_name EleutherAI/gpt-j-6B --mask_filling_model_name t5-3b --n_perturbation_list 1,10,100 --n_samples 500 --pct_words_masked 0.3 --span_length 2 --dataset writing
-#python run.py --output_name main --base_model_name facebook/opt-2.7b --mask_filling_model_name t5-3b --n_perturbation_list 1,10,100 --n_samples 500 --pct_words_masked 0.3 --span_length 2 --dataset writing
-#python run.py --output_name main --batch_size 20 --base_model_name EleutherAI/gpt-neox-20b --mask_filling_model_name t5-11b --n_perturbation_list 1,10,100 --n_samples 500 --pct_words_masked 0.3 --span_length 2 --dataset writing
-#
-#python run.py --output_name main_top_p --base_model_name gpt2-xl --mask_filling_model_name t5-3b --n_perturbation_list 1,10,100 --n_samples 500 --pct_words_masked 0.3 --span_length 2 --do_top_p --dataset writing
-#python run.py --output_name main_top_p --base_model_name EleutherAI/gpt-neo-2.7B --mask_filling_model_name t5-3b --n_perturbation_list 1,10,100 --n_samples 500 --pct_words_masked 0.3 --span_length 2 --do_top_p --dataset writing
-#python run.py --output_name main_top_p --base_model_name EleutherAI/gpt-j-6B --mask_filling_model_name t5-3b --n_perturbation_list 1,10,100 --n_samples 500 --pct_words_masked 0.3 --span_length 2 --do_top_p --dataset writing
-#python run.py --output_name main_top_p --base_model_name facebook/opt-2.7b --mask_filling_model_name t5-3b --n_perturbation_list 1,10,100 --n_samples 500 --pct_words_masked 0.3 --span_length 2 --do_top_p --dataset writing
-#python run.py --output_name main_top_p --batch_size 20 --base_model_name EleutherAI/gpt-neox-20b --mask_filling_model_name t5-11b --n_perturbation_list 1,10,100 --n_samples 500 --pct_words_masked 0.3 --span_length 2 --do_top_p --dataset writing
-#
-#python run.py --output_name main_top_k --base_model_name gpt2-xl --mask_filling_model_name t5-3b --n_perturbation_list 1,10,100 --n_samples 500 --pct_words_masked 0.3 --span_length 2 --do_top_k --dataset writing
-#python run.py --output_name main_top_k --base_model_name EleutherAI/gpt-neo-2.7B --mask_filling_model_name t5-3b --n_perturbation_list 1,10,100 --n_samples 500 --pct_words_masked 0.3 --span_length 2 --do_top_k --dataset writing
-#python run.py --output_name main_top_k --base_model_name EleutherAI/gpt-j-6B --mask_filling_model_name t5-3b --n_perturbation_list 1,10,100 --n_samples 500 --pct_words_masked 0.3 --span_length 2 --do_top_k --dataset writing
-#python run.py --output_name main_top_k --base_model_name facebook/opt-2.7b --mask_filling_model_name t5-3b --n_perturbation_list 1,10,100 --n_samples 500 --pct_words_masked 0.3 --span_length 2 --do_top_k --dataset writing
-#python run.py --output_name main_top_k --batch_size 20 --base_model_name EleutherAI/gpt-neox-20b --mask_filling_model_name t5-11b --n_perturbation_list 1,10,100 --n_samples 500 --pct_words_masked 0.3 --span_length 2 --do_top_k --dataset writing
+!python run.py --base_model_name gpt2 --mask_filling_model_name t5-small --pct_words_masked 0.3 --skip_baselines --n_samples 200 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models --output_name experiment1
+!python run.py --base_model_name gpt2 --mask_filling_model_name t5-small --pct_words_masked 0.5 --skip_baselines --n_samples 200 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models --output_name experiment2
+!python run.py --base_model_name gpt2 --mask_filling_model_name t5-large --pct_words_masked 0.3 --skip_baselines --n_samples 200 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models --output_name experiment3
+!python run.py --base_model_name gpt2 --mask_filling_model_name t5-large --pct_words_masked 0.5 --skip_baselines --n_samples 200 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models --output_name experiment4
+!python run.py --base_model_name gpt2 --mask_filling_model_name t5-3b --pct_words_masked 0.3 --skip_baselines --n_samples 200 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models --output_name experiment5
+!python run.py --base_model_name gpt2 --mask_filling_model_name t5-3b --pct_words_masked 0.5 --skip_baselines --n_samples 200 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models --output_name experiment6
+!python run.py --base_model_name gpt2-medium --mask_filling_model_name t5-small --pct_words_masked 0.3 --skip_baselines --n_samples 200 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models --output_name experiment7
+!python run.py --base_model_name gpt2-medium --mask_filling_model_name t5-small --pct_words_masked 0.5 --skip_baselines --n_samples 200 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models --output_name experiment8
+!python run.py --base_model_name gpt2-medium --mask_filling_model_name t5-large --pct_words_masked 0.3 --skip_baselines --n_samples 200 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models --output_name experiment9
+!python run.py --base_model_name gpt2-medium --mask_filling_model_name t5-large --pct_words_masked 0.5 --skip_baselines --n_samples 200 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models --output_name experiment10
+!python run.py --base_model_name gpt2-medium --mask_filling_model_name t5-3b --pct_words_masked 0.3 --skip_baselines --n_samples 200 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models --output_name experiment11
+!python run.py --base_model_name gpt2-medium --mask_filling_model_name t5-3b --pct_words_masked 0.5 --skip_baselines --n_samples 200 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models --output_name experiment12
+!python run.py --base_model_name gpt2-large --mask_filling_model_name t5-small --pct_words_masked 0.3 --skip_baselines --n_samples 200 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models --output_name experiment13
+!python run.py --base_model_name gpt2-large --mask_filling_model_name t5-small --pct_words_masked 0.5 --skip_baselines --n_samples 200 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models --output_name experiment14
+!python run.py --base_model_name gpt2-large --mask_filling_model_name t5-large --pct_words_masked 0.3 --skip_baselines --n_samples 200 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models --output_name experiment15
+!python run.py --base_model_name gpt2-large --mask_filling_model_name t5-large --pct_words_masked 0.5 --skip_baselines --n_samples 200 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models --output_name experiment16
+!python run.py --base_model_name gpt2-large --mask_filling_model_name t5-3b --pct_words_masked 0.3 --skip_baselines --n_samples 200 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models --output_name experiment17
+!python run.py --base_model_name gpt2-large --mask_filling_model_name t5-3b --pct_words_masked 0.5 --skip_baselines --n_samples 200 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models --output_name experiment18
--- paper_scripts/one.sh
+++ paper_scripts/one.sh
@@ -2,79 +2,60 @@
 
 case $1 in
 1)
-python run.py --base_model_name gpt2 --mask_filling_model_name t5-small --pct_words_masked 0.3 --skip_baselines --n_samples 500 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models
+python run.py --base_model_name gpt2 --mask_filling_model_name t5-small --pct_words_masked 0.3 --skip_baselines --n_samples 200 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models --output_name experiment1
 ;;
 2)
-python run.py --base_model_name gpt2 --mask_filling_model_name t5-small --pct_words_masked 0.5 --skip_baselines --n_samples 500 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models
+python run.py --base_model_name gpt2 --mask_filling_model_name t5-small --pct_words_masked 0.5 --skip_baselines --n_samples 200 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models --output_name experiment2
 ;;
 3)
-python run.py --base_model_name gpt2 --mask_filling_model_name t5-large --pct_words_masked 0.3 --skip_baselines --n_samples 500 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models
+python run.py --base_model_name gpt2 --mask_filling_model_name t5-large --pct_words_masked 0.3 --skip_baselines --n_samples 200 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models --output_name experiment3
 ;;
 4)
-python run.py --base_model_name gpt2 --mask_filling_model_name t5-large --pct_words_masked 0.5 --skip_baselines --n_samples 500 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models
+python run.py --base_model_name gpt2 --mask_filling_model_name t5-large --pct_words_masked 0.5 --skip_baselines --n_samples 200 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models --output_name experiment4
 ;;
 5)
-python run.py --base_model_name gpt2 --mask_filling_model_name t5-3b --pct_words_masked 0.3 --skip_baselines --n_samples 500 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models
+python run.py --base_model_name gpt2 --mask_filling_model_name t5-3b --pct_words_masked 0.3 --skip_baselines --n_samples 200 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models --output_name experiment5
 ;;
 6)
-python run.py --base_model_name gpt2 --mask_filling_model_name t5-3b --pct_words_masked 0.5 --skip_baselines --n_samples 500 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models
+python run.py --base_model_name gpt2 --mask_filling_model_name t5-3b --pct_words_masked 0.5 --skip_baselines --n_samples 200 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models --output_name experiment6
 ;;
 7)
-python run.py --base_model_name gpt2-medium --mask_filling_model_name t5-small --pct_words_masked 0.3 --skip_baselines --n_samples 500 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models
+python run.py --base_model_name gpt2-medium --mask_filling_model_name t5-small --pct_words_masked 0.3 --skip_baselines --n_samples 200 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models --output_name experiment7
 ;;
 8)
-python run.py --base_model_name gpt2-medium --mask_filling_model_name t5-small --pct_words_masked 0.5 --skip_baselines --n_samples 500 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models
+python run.py --base_model_name gpt2-medium --mask_filling_model_name t5-small --pct_words_masked 0.5 --skip_baselines --n_samples 200 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models --output_name experiment8
 ;;
 9)
-python run.py --base_model_name gpt2-medium --mask_filling_model_name t5-large --pct_words_masked 0.3 --skip_baselines --n_samples 500 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models
+python run.py --base_model_name gpt2-medium --mask_filling_model_name t5-large --pct_words_masked 0.3 --skip_baselines --n_samples 200 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models --output_name experiment9
 ;;
 10)
-python run.py --base_model_name gpt2-medium --mask_filling_model_name t5-large --pct_words_masked 0.5 --skip_baselines --n_samples 500 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models
+python run.py --base_model_name gpt2-medium --mask_filling_model_name t5-large --pct_words_masked 0.5 --skip_baselines --n_samples 200 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models --output_name experiment10
 ;;
 11)
-python run.py --base_model_name gpt2-medium --mask_filling_model_name t5-3b --pct_words_masked 0.3 --skip_baselines --n_samples 500 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models
+python run.py --base_model_name gpt2-medium --mask_filling_model_name t5-3b --pct_words_masked 0.3 --skip_baselines --n_samples 200 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models --output_name experiment11
 ;;
 12)
-python run.py --base_model_name gpt2-medium --mask_filling_model_name t5-3b --pct_words_masked 0.5 --skip_baselines --n_samples 500 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models
+python run.py --base_model_name gpt2-medium --mask_filling_model_name t5-3b --pct_words_masked 0.5 --skip_baselines --n_samples 200 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models --output_name experiment12
 ;;
 13)
-python run.py --base_model_name gpt2-large --mask_filling_model_name t5-small --pct_words_masked 0.3 --skip_baselines --n_samples 500 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models
+python run.py --base_model_name gpt2-large --mask_filling_model_name t5-small --pct_words_masked 0.3 --skip_baselines --n_samples 200 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models --output_name experiment13
 ;;
 14)
-python run.py --base_model_name gpt2-large --mask_filling_model_name t5-small --pct_words_masked 0.5 --skip_baselines --n_samples 500 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models
+python run.py --base_model_name gpt2-large --mask_filling_model_name t5-small --pct_words_masked 0.5 --skip_baselines --n_samples 200 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models --output_name experiment14
 ;;
 15)
-python run.py --base_model_name gpt2-large --mask_filling_model_name t5-large --pct_words_masked 0.3 --skip_baselines --n_samples 500 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models
+python run.py --base_model_name gpt2-large --mask_filling_model_name t5-large --pct_words_masked 0.3 --skip_baselines --n_samples 200 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models --output_name experiment15
 ;;
 16)
-python run.py --base_model_name gpt2-large --mask_filling_model_name t5-large --pct_words_masked 0.5 --skip_baselines --n_samples 500 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models
+python run.py --base_model_name gpt2-large --mask_filling_model_name t5-large --pct_words_masked 0.5 --skip_baselines --n_samples 200 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models --output_name experiment16
 ;;
 17)
-python run.py --base_model_name gpt2-large --mask_filling_model_name t5-3b --pct_words_masked 0.3 --skip_baselines --n_samples 500 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models
+python run.py --base_model_name gpt2-large --mask_filling_model_name t5-3b --pct_words_masked 0.3 --skip_baselines --n_samples 200 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models --output_name experiment17
 ;;
 18)
-python run.py --base_model_name gpt2-large --mask_filling_model_name t5-3b --pct_words_masked 0.5 --skip_baselines --n_samples 500 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models
+python run.py --base_model_name gpt2-large --mask_filling_model_name t5-3b --pct_words_masked 0.5 --skip_baselines --n_samples 200 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models --output_name experiment18
 ;;
 *)
 echo "Invalid experiment number. must be in range 1-18"
 ;;
 esac
-
-#!python run.py --base_model_name gpt2 --mask_filling_model_name t5-small --pct_words_masked 0.3 --skip_baselines --n_samples 500 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models
-#!python run.py --base_model_name gpt2 --mask_filling_model_name t5-small --pct_words_masked 0.5 --skip_baselines --n_samples 500 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models
-#!python run.py --base_model_name gpt2 --mask_filling_model_name t5-large --pct_words_masked 0.3 --skip_baselines --n_samples 500 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models
-#!python run.py --base_model_name gpt2 --mask_filling_model_name t5-large --pct_words_masked 0.5 --skip_baselines --n_samples 500 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models
-#!python run.py --base_model_name gpt2 --mask_filling_model_name t5-3b --pct_words_masked 0.3 --skip_baselines --n_samples 500 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models
-#!python run.py --base_model_name gpt2 --mask_filling_model_name t5-3b --pct_words_masked 0.5 --skip_baselines --n_samples 500 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models
-#!python run.py --base_model_name gpt2-medium --mask_filling_model_name t5-small --pct_words_masked 0.3 --skip_baselines --n_samples 500 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models
-#!python run.py --base_model_name gpt2-medium --mask_filling_model_name t5-small --pct_words_masked 0.5 --skip_baselines --n_samples 500 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models
-#!python run.py --base_model_name gpt2-medium --mask_filling_model_name t5-large --pct_words_masked 0.3 --skip_baselines --n_samples 500 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models
-#!python run.py --base_model_name gpt2-medium --mask_filling_model_name t5-large --pct_words_masked 0.5 --skip_baselines --n_samples 500 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models
-#!python run.py --base_model_name gpt2-medium --mask_filling_model_name t5-3b --pct_words_masked 0.3 --skip_baselines --n_samples 500 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models
-#!python run.py --base_model_name gpt2-medium --mask_filling_model_name t5-3b --pct_words_masked 0.5 --skip_baselines --n_samples 500 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models
-#!python run.py --base_model_name gpt2-large --mask_filling_model_name t5-small --pct_words_masked 0.3 --skip_baselines --n_samples 500 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models
-#!python run.py --base_model_name gpt2-large --mask_filling_model_name t5-small --pct_words_masked 0.5 --skip_baselines --n_samples 500 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models
-#!python run.py --base_model_name gpt2-large --mask_filling_model_name t5-large --pct_words_masked 0.3 --skip_baselines --n_samples 500 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models
-#!python run.py --base_model_name gpt2-large --mask_filling_model_name t5-large --pct_words_masked 0.5 --skip_baselines --n_samples 500 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models
-#!python run.py --base_model_name gpt2-large --mask_filling_model_name t5-3b --pct_words_masked 0.3 --skip_baselines --n_samples 500 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models
-#!python run.py --base_model_name gpt2-large --mask_filling_model_name t5-3b --pct_words_masked 0.5 --skip_baselines --n_samples 500 --n_perturbation_list 10,15,20 --dataset reviews --cache_dir models
